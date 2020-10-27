a3fba52ea50466f3d7ee78d15331f67b87e21f99
PANACHAI THONGVINIT <jameslovepe@hotmail.com>
2020-10-27 17:15:09 +0000

    lab complete

--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-export class MoviesItem extends React.Component{
+export class MoviesItem extends React.Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-        {/*
+                {/*
             <h3>{this.props.movie.Title}</h3>
             <p>{this.props.movie.Year}</p>
             <img src={this.props.movie.Poster} width="200" height="200"></img>
         */}
-        <Card>
-  <Card.Header>{this.props.movie.Title}</Card.Header>
-  <Card.Body>
-    <blockquote className="blockquote mb-0">
-    <img src={this.props.movie.Poster} width="200" height="200"></img>
-      <footer className="blockquote-footer">
-      <p>{this.props.movie.Year}</p>
-      </footer>
-    </blockquote>
-  </Card.Body>
-</Card>
+                <Card>
+                    <Card.Header>{this.props.movie.Title}</Card.Header>
+                    <Card.Body>
+                        <blockquote className="blockquote mb-0">
+                            <img src={this.props.movie.Poster} width="200" height="200"></img>
+                            <footer className="blockquote-footer">
+                                <p>{this.props.movie.Year}</p>
+                            </footer>
+                        </blockquote>
+                    </Card.Body>
+                </Card>
             </div>
-            
+
         );
     }
 }
